bf08cfb1f222f2794c5c2aac421d011d4d8b9e37
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-08-12 12:28:19 +0200

    add all hw-03

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,33 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody, checkBody } = require("../../middelwares");
+const { validateBody, checkBody, isValidId } = require("../../middelwares");
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:id", ctrl.getContactById);
+router.get("/:id", isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+router.post("/", checkBody, validateBody(schemas.addSchema), ctrl.addContact);
 
-router.put("/:id",checkBody, validateBody(schemas.addSchema), ctrl.updateContact);
+router.delete("/:id", isValidId, ctrl.removeContact);
 
-router.delete("/:id", ctrl.removeContact);
+router.put(
+  "/:id",
+  isValidId,
+  checkBody,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
+
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
